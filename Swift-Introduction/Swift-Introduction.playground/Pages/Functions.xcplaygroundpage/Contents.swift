50fa1a3eb4423cabfb4647c6989ba03a01e8c656
TeYenWu <teyanwu@gmail.com>
2018-02-23 03:23:30 +0800

    Add Nested Function to "Function"

--- Swift-Introduction/Swift-Introduction.playground/Pages/Functions.xcplaygroundpage/Contents.swift
+++ Swift-Introduction/Swift-Introduction.playground/Pages/Functions.xcplaygroundpage/Contents.swift
@@ -240,7 +240,33 @@ swapTwoNumbers(&firstDouble, &secondDouble)
 
 //: --------------------------------------------------------------------------------------------------------------------
 /*:
- ### Arguments with Generic Types
+ 
+ ### Nested Functions
+ 
+ All of the functions you have encountered so far in this chapter have been examples of global functions, which are defined at a global scope. You can also define functions inside the bodies of other functions, known as nested functions.
+ 
+ Nested functions are hidden from the outside world by default, but can still be called and used by their enclosing function. An enclosing function can also return one of its nested functions to allow the nested function to be used in another scope.
+ 
+ You can rewrite the chooseStepFunction(backward:) example above to use and return nested functions:
+ */
+func chooseStepFunction(backward: Bool) -> (Int) -> Int {
+    func stepForward(input: Int) -> Int { return input + 1 }
+    func stepBackward(input: Int) -> Int { return input - 1 }
+    return backward ? stepBackward : stepForward
+}
+
+var currentValue = -4
+let moveNearerToZero = chooseStepFunction(backward: currentValue > 0)
+// moveNearerToZero now refers to the nested stepForward() function
+while currentValue != 0 {
+    print("\(currentValue)... ")
+    currentValue = moveNearerToZero(currentValue)
+}
+print("zero!")
+
+//: --------------------------------------------------------------------------------------------------------------------
+/*:
+ ### (Self-Reading) Arguments with Generic Types
  
  Generics provides the ability to have a single implementation that works for all possible types.
  
